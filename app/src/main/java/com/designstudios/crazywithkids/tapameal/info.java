dae9da4ee63c35e2971595793c3bf780d90a3dd5
dmonreal <hannahmisssmomma@gmail.com>
2018-06-09 10:19:52 -0700

    reference card and details created

--- app/src/main/java/com/designstudios/crazywithkids/tapameal/info.java
+++ app/src/main/java/com/designstudios/crazywithkids/tapameal/info.java
@@ -1,15 +1,52 @@
 package com.designstudios.crazywithkids.tapameal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class info extends AppCompatActivity {
+
+    Button proteinPopBtn,fatPopBtn,dairyPopBtn, veggiePopBtn, fruitPopBtn, startchPopBtn,
+            treatPopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        proteinPopBtn = findViewById(R.id.p8);
+        Intent intent = new Intent(this, proteinPop.class);
+        startActivity(intent);
 
+        dairyPopBtn = findViewById(R.id.d);
+        Intent dintent = new Intent(this, proteinPop.class);
+        startActivity(dintent);
+
+        fatPopBtn = findViewById(R.id.fat);
+        Intent fatintent = new Intent(this, proteinPop.class);
+        startActivity(fatintent);
+
+        veggiePopBtn = findViewById(R.id.v);
+        Intent vintent = new Intent(this, proteinPop.class);
+        startActivity(vintent);
+
+        fruitPopBtn = findViewById(R.id.f);
+        Intent fintent = new Intent(this, proteinPop.class);
+        startActivity(fintent);
+
+        startchPopBtn = findViewById(R.id.s9);
+        Intent sintent = new Intent(this, proteinPop.class);
+        startActivity(sintent);
+
+        treatPopBtn = findViewById(R.id.t);
+        Intent tintent = new Intent(this, proteinPop.class);
+        startActivity(tintent);
+
+    }
+    public void home(View view){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/designstudios/crazywithkids/tapameal/veggiePop.java
@@ -0,0 +1,41 @@
+package com.designstudios.crazywithkids.tapameal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+public class veggiePop extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.veggiepopwindow);
+
+        DisplayMetrics DM = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(DM);
+
+        int width = DM.widthPixels;
+        int height = DM.heightPixels;
+
+        getWindow().setLayout((int)(width*.75),(int)(height*.75));
+
+
+
+    }
+
+    public void source(View view) {
+
+        Intent sourceIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://members.tripod" +
+                ".com/sue_in_nj/exchanges.htm"));
+        startActivity(sourceIntent);
+    }
+
+    public void close(View view) {
+        finish();
+    }
+}
